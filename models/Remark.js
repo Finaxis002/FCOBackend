ae66a0c57b3b5a722013fb93032ad499f4638698
finaxis002 <finaxis.ai@gmail.com>
2025-05-21 17:58:36 +0530

    remark require fixes

--- models/Remark.js
+++ models/Remark.js
@@ -6,7 +6,7 @@ const remarkSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   userName: { type: String, required: true },
   remark: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model("Remark", remarkSchema);
